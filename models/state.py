512464296ca3f886f6a682fa0e8534547e5e6a0f
Gorileng <keneilwe963@gmail.com>
2023-09-18 20:19:40 -0700

    updated

--- models/state.py
+++ models/state.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+""" State Module for the HBNB project """
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """ State class """
+    """ A State class """
     name = ""
