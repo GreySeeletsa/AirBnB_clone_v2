93b7cd7eab3008a07c5e1385daf4486a6420c597
GreySeeletsa <seeletsagrey@gmail.com>
2023-10-20 11:27:33 -0700

    script that starts a Flask web application

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+
+# import Flask class
+from flask import Flask
+
+# create an instance called webapp
+webapp = Flask(__name__)
+webapp.url_map.strict_slashes = False
+
+
+@webapp.route('/')
+def index():
+    """To return Hello HBNB
+    """
+    return 'Hello HBNB!'
+
+
+@webapp.route('/hbnb')
+def hbnb_route():
+    """To return"HBNB"
+    """
+    return 'HBNB'
+
+
+@webapp.route('/c/<text>')
+def routefor_C(text):
+    """display "C", followed by the value of the text variable
+    """
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    webapp.run(debug=True)
